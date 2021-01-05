301f404072976e685ab9756258b566437b9a93f6
shinedo <shinedo@foxmail.com>
2021-01-06 01:00:27 +0800

    无

--- Assets/Scripts/TextCryptography/DataInfo.cs
+++ Assets/Scripts/TextCryptography/DataInfo.cs
@@ -3,27 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataInfo
+#region 武器数据模型
+
+[Serializable]
+[DataPath(FileManager.dataFolder+"weapon")]
+public class  WeaponInfo
 {
-    #region 武器数据模型
-
-    [Serializable]
-
-    public class  WeaponInfo
-    {
-        public int Id;
-        public int Type;
-        public string Name;
-        public string Description;
-        public string Resources;
-        public string Quality;
-        public int Attack;
-        public int UpgradeAttack;
-        public int Magazine;
-        public int Range;
-        public int Speed;
-        public float RateOfFire;
-    }
-
-    #endregion
+    public int Id;
+    public int Type;
+    public string Name;
+    public string Description;
+    public string Resources;
+    public string Quality;
+    public int Attack;
+    public int UpgradeAttack;
+    public int Magazine;
+    public int Range;
+    public int Speed;
+    public float RateOfFire;
 }
+
+#endregion
+
+#region 用户数据
+
+[Serializable]
+public class UserInfo
+{
+    public int Id;
+    public string Name;
+    public int Age;
+    public string Sex;
+}
+
+#endregion
---
+++ Assets/Scripts/TextCryptography/FileManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileManager : MonoBehaviour
+{
+    private static bool _initAssetBundle;
+    
+    //静态数据
+    private static List<WeaponInfo> _weaponInfoList = new List<WeaponInfo>();
+    //数据文件位置
+    public const string dataFolder = "/data/";
+    //玩家数据
+    public static UserInfo mUserInfo;
+
+    private static bool isDataInit;
+
+    public static void Init()
+    {
+        if(isDataInit)
+            return;
+        isDataInit = true;
+        InitBundle();
+    }
+
+    public static void InitBundle()
+    {
+        if (_initAssetBundle)
+        {
+            _initAssetBundle = true;
+            
+            
+        }
+    }
+
+    public static void ParserFromTxtFile<T>(List<T> list, bool refResource = false)
+    {
+        string asset = null;
+        
+        //获取文件路径
+        string file = ((DataPathAttribute) Attribute.GetCustomAttribute(typeof(T), typeof(DataPathAttribute))).filePath;
+        
+        
+    }
+    
+}
+
+/// <summary>
+/// 注释，各个数据对应的文件
+/// </summary>
+[AttributeUsage(AttributeTargets.Class,AllowMultiple = false,Inherited = false)]
+public class DataPathAttribute : Attribute
+{
+    public string filePath { get; set; }
+
+    public DataPathAttribute(string _filePath)
+    {
+        filePath = _filePath;
+    }
+}
